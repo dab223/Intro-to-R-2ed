582ec9afec5651a10827a6c09681a0cf3f327080
Danyal Burhan <danyal.burhan@gmail.com>
2020-08-21 15:13:13 -0400

    RStudio Push
    Playing around with '#' and comment lines

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
 # R script to accompany Intro to R
-# Copy for Danyal Burhan 2019
+# Copy for Danyal Burhan, '23 2019
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
-# in your script by putting "#" at the beginning of the lines.
-# 
+# Bachelor's Degree, Lehigh University
+# College of Business
+#
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
