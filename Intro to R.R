26c5c52301b2c34337d8fe1d35ea22d531d38afa
Danyal Burhan <dab223@lehigh.edu>
2020-08-24 13:58:33 -0400

    RStudio Message Modification
    Playing around with commits and staging commits.

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
 # R script to accompany Intro to R
-# Copy for Danyal Burhan, '23 2019
+# Copy for Danyal Burhan, Finance '23 
 ######################################################
 
-# Bachelor's Degree, Lehigh University
+# Bachelor's Degree, Lehigh University 
 # College of Business
-#
+# 
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
